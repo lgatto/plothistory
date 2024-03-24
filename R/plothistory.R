41ee51711f9381aaca5acdcaab14f49a3181cd97
Laurent Gatto <laurent.gatto@uclouvain.be>
2024-03-24 22:19:58 +0100

    fix - now working again

--- R/plothistory.R
+++ R/plothistory.R
@@ -3,53 +3,53 @@
 ##' @param phdir `character(1)` defining the plothistory
 ##'     directory. Default is to [phist_tmp_dir()].
 ##'
-##' @param sleep `numeric(1)` > 0 indicating the clock cycle in
-##'     seconds. at which `phdir` should be updated. Default is 1
-##'     second. Passed to [Sys.sleep()].
-##'
-##' @param hgd `logical(1)` defining whether a web graphics device
-##'     should be started with `port` and `token`. Default is
-##'     `TRUE`. In case the backgroup script stops, the plotting
-##'     history can be restarted by setting `hgd = FALSE`.
-##'
-##' @param port `numeric(1)` defining the server port, passed to
-##'     [httpgd::hgd()]. If this is set to ‘0’, an open port will be
-##'     assigned. Default is 5900.
-##'
-##' @param token Optional security token, passed to
-##'     [httpgd::hgd()]. Default is `FALSE` to deactivate the token.
-##'
-##' @param ... Additional parameters passed to [httpgd::hgd()].
-##'
 ##' @export
 ##'
 ##' @importFrom httpgd hgd
 ##'
 ##' @return The value of the error code (0 for success) of the
-##'     background script. See [system2()] for details.
-plothistory <- function(phdir = phist_tmp_dir(),
-                        sleep = 1,
-                        hgd = TRUE, 
-                        port = 5900,
-                        token = FALSE,
-                        ...) {
+##'     background script.
+plothistory <- function(phdir = phist_tmp_dir()) {
     force(phdir)
-    if (hgd) {
-        hgd(port = port, token = token, ...)
-        plot(1, type = "n", axes = FALSE, xlab = NA, ylab = NA)
-    }
+    phdir <- path.expand(phdir)
+    stopifnot(dir.exists(phdir))
+
+    httpgd::hgd(port = 5900, token = FALSE)
+    plot(1, type = "n", axes = FALSE, xlab = NA, ylab = NA)    
     script <- system.file("scripts",
                           package = "plothistory",
-                          pattern = "plothistory.R")
-    args <- c(script, phdir, sleep)
-    system2("Rscript", args = args, wait = FALSE)
+                          pattern = "plothistory.R")    
+    cmd <- paste("Rscript", script, phdir)
+    ## message(cmd)
+    system(cmd, wait = FALSE)
 }
 
-## called in script/plothistory.R
-run_plothistory <- function(phdir, sleep) {
-    on.exit(httpgd::hgd_close())
-    while(TRUE) {
-        save_plot_to_file(get_plot(), phdir)
-        Sys.sleep(sleep)
-    }
+
+##' @param dir `character(1)` with the path to the plothistory
+##'     directory. See [phist_dir()] for details.
+##'
+##' @param lnsym `character(1)` with the symbolic link's name, that
+##'     will point to the last plot filename.
+##'
+##' @return `character(1)` containing the newly created filename or
+##'     `NULL`, if `x` was `length(x) == 0`.
+##'
+##' @noRd
+##'
+##' @importFrom tools md5sum
+save_plot_to_file <- function(dir, lnsym = ".last.svg") {
+    dir <- path.expand(dir)
+    lnsym <- file.path(dir, lnsym)
+    stopifnot(dir.exists(dir))
+    fl <- file.path(dir, "current.svg")
+    svg <- readLines("http://127.0.0.1:5900/plot", warn = FALSE)    
+    writeLines(svg, fl)
+    on.exit(unlink(fl))    
+    md5 <- tools::md5sum(fl)    
+    newf <- file.path(dir, paste0(md5[[1]], ".svg"))
+    file.rename(from = fl, to = newf)
+    unlink(lnsym)
+    file.symlink(newf, lnsym)
+    invisible(newf)
 }
+
--- inst/scripts/plothistory.R
+++ inst/scripts/plothistory.R
@@ -2,12 +2,8 @@ args <- commandArgs(trailingOnly = TRUE)
 
 ## check plothistory diretory
 phdir <- args[1]
-phdir <- path.expand(phdir)
-stopifnot(dir.exists(phdir))
-    
-## update every sleep seconds    
-sleep <- as.numeric(args[2])
-stopifnot(!is.na(sleep),
-          sleep > 0)
 
-plothistory:::run_plothistory(phdir, sleep)
+while(TRUE) {
+    plothistory:::save_plot_to_file(phdir)
+    Sys.sleep(1)
+}

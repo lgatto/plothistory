e216ba63f87d7195d4d67b2753257eb138787bbc
Laurent Gatto <laurent.gatto@uclouvain.be>
2024-03-25 11:23:53 +0100

    Message with num and size of files in cache (when > 10 Mb)

--- R/cache.R
+++ R/cache.R
@@ -8,6 +8,9 @@
 ##'
 ##' When plots are cached, they are named after their md5sum.
 ##'
+##' The function will also display a message with the number of files
+##' and total size used when the latter reaches 10 Mb.
+##'
 ##' @param ask `logical(1)` that defines whether to ask to create the
 ##'     cache directory it doesn't exist yet. Default is `TRUE`.
 ##' 
@@ -29,7 +32,7 @@
 ##' phist_cache(ask = FALSE)
 phist_cache <- function(ask = TRUE) {
     cache <- tools::R_user_dir(package = "plothistory",
-                               which = "cache")
+                               which = "cache")    
     if (!file.exists(cache)) {
         if (!interactive()) {
             if (ask) return(NA)
@@ -48,5 +51,16 @@ phist_cache <- function(ask = TRUE) {
         if (doit)
             dir.create(cache, recursive = TRUE)
     }
+    flzs_msg(cache)
     return(cache)
+}
+
+flzs_msg <- function(dir, verbose = TRUE, size_msg = 10) {
+    fls <- list.files(dir, full.names = TRUE)
+    sz <- sum(file.size(fls))
+    sz <- round(sz / 1e6, 2)
+    if (verbose & (sz > size_msg))
+        message(dir, " contains ", length(fls),
+                " files and uses ", sz, " Mb")
+    invisible(sz)
 }

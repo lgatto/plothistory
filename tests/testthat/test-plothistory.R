566615e6327c7fecad074d8697e55902b848c8cc
Laurent Gatto <laurent.gatto@uclouvain.be>
2024-03-27 17:08:44 +0100

    need to wait in test

--- tests/testthat/test-plothistory.R
+++ tests/testthat/test-plothistory.R
@@ -1,13 +1,9 @@
 test_that("works with relative filename (issue #2)", {
-    ## this fails on GHA, not sure why
     tf <- tempdir()
     setwd(tf)
     plothistory(phist_dir("figs"))
     plot(1)
-    message(tf)
-    message(getwd())
-    message(paste(dir(full.names = TRUE, all.files = TRUE),
-                  collapse = "\n"))
+    Sys.sleep(2) ## wait to make sure the file is created
     expect_true(file.exists("figs/.last.svg"))
     ## close device
     httpgd::hgd_close()

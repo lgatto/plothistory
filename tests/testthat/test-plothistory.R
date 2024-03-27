9b43b5093aaad62da4dbc4b790dd4bd914640312
Laurent Gatto <laurent.gatto@uclouvain.be>
2024-03-27 15:54:41 +0100

    debugging gha

--- tests/testthat/test-plothistory.R
+++ tests/testthat/test-plothistory.R
@@ -1,7 +1,11 @@
 test_that("works with relative filename (issue #2)", {
+    ## this fails on GHA, not sure why
     tf <- tempdir()
     setwd(tf)
+    message(tf)
     plothistory(phist_dir("figs"))
     plot(1)
     expect_true(file.exists("figs/.last.svg"))
+    ## close device
+    httpgd::hgd_close()
 })

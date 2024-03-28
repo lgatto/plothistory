dd212a38ed762863b44aceb6020eff4d8bf6ee38
Laurent Gatto <laurent.gatto@uclouvain.be>
2024-03-28 07:53:37 +0100

    testing ws

--- inst/scripts/ws.R
+++ inst/scripts/ws.R
@@ -1,21 +1,35 @@
 library(httpgd)
+library(unigd)
 library(websocket)
 
-hgd(token = FALSE)
+hgd()
 
-wskt <- paste0(
-    "ws://", hgd_details()$host,
-    ":", hgd_details()$port)
+(wskt <- paste0(
+     "ws://", hgd_details()$host,
+     ":", hgd_details()$port))
+
 
 ws <- websocket::WebSocket$new(wskt ,
                                autoConnect = FALSE)
 
 ws$onOpen(function(event) {
-  cat("Connection opened\n")
+    cat("Connection opened\n")
+    cat(event$data, "\n")
 })
 
 ws$onMessage(function(event) {
-    cat("Client got msg: ", event$data, "\n")
+    ## cat("Client got msg: ", event$data, "\n")
+    hs <- jsonlite::parse_json(event$data)$hsize
+    n <- get("n")
+    ## message("Current is ", n, "\n")
+    if (hs == (n + 1))
+        message("New plot")
+    else if (hs == n) {} ## Nothing
+    else if (hs < n)
+        message("Plot(s) deleted")
+    else
+        warning("What happened?")
+    n <<- hs
 })
 
 ws$onClose(function(event) {
@@ -27,4 +41,12 @@ ws$onError(function(event) {
   cat("Client failed to connect: ", event$message, "\n")
 })
 
+n <- 0
+
 ws$connect()
+
+
+## Need to keep strat when hsize increases and decreases, as messages
+## are returned when new plots are created (and hsize is incremented),
+## when a plot is deleted (and hsize is decremented), when one zooms
+## in and out, when history is hidden,

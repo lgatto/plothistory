fb5530a70bd82a500e96b48709d1c8d3d944cd01
Laurent Gatto <laurent.gatto@uclouvain.be>
2024-03-29 17:45:42 +0100

    trying to fix/understant unit test

--- tests/testthat/test-plothistory.R
+++ tests/testthat/test-plothistory.R
@@ -1,48 +1,27 @@
 ## test_that("works with absolute filenames", {
-##     wd <- getwd()
-##     (f <- file.path(wd, "figs"))
-##     (ans <- file.path(wd, "figs/.last.svg"))
-##     plothistory(phist_dir(f))
-##     plot(1)
-##     expect_true(file.exists(ans))
-##     plothistory(NULL)
-##     unlink("figs", recursive = TRUE, force = TRUE)
+##     withr::with_dir(tempdir(),  {
+##         ## working in temp directory
+##         wd <- getwd()
+##         f <- file.path(wd, "figs")
+##         ans <- file.path(wd, "figs/.last.svg")
+##         plothistory(phist_dir(f))
+##         plot(rnorm(100))
+##         Sys.sleep(2)
+##         print(res <- file.exists(ans))
+##         expect_true(TRUE)
+##         plothistory(NULL)
+##     })
 ## })
 
-## test_that("works with relative filenames", {
-##     wd <- getwd()
-##     f <- "figs"
-##     (ans <- file.path(wd, "figs/.last.svg"))
-##     plothistory(phist_dir(f))
-##     plot(1)
-##     expect_true(file.exists(ans))
-##     plothistory(NULL)
-##     unlink("figs", recursive = TRUE, force = TRUE)
-## })
-
-
-## test_that("works with absolute relative filenames", {
-##     td <- tempdir(check = TRUE)
-##     setwd(td)
-##     f <- file.path(td, "figs")
-##     ans <- file.path(td, "figs/.last.svg")
-##     plothistory(phist_dir(f))
-##     plot(1)
-##     expect_true(file.exists(ans))
-##     plothistory(NULL)
-##     unlink(td, recursive = TRUE, force = TRUE)
-##     httpgd::hgd_close()
-## })
-
-
-## test_that("works with relative filename (issue #2)", {
-##     td <- tempdir(check = TRUE)
-##     setwd(td)
-##     f <- "figs"
-##     ans <- file.path(td, "figs/.last.svg")
-##     plothistory(phist_dir(f))
-##     plot(1)
-##     expect_true(file.exists(ans))
-##     plothistory(NULL)
-##     httpgd::hgd_close()
+## test_that("works with relative filenames (issue #2)", {
+##     withr::with_dir(tempdir(),  {
+##         ## working in temp directory
+##         wd <- getwd()
+##         f <- "figs"
+##         ans <- file.path(wd, "figs/.last.svg")
+##         plothistory(phist_dir(f))
+##         plot(rnorm(100))
+##         expect_true(file.exists(ans))
+##         plothistory(NULL)
+##     })
 ## })
